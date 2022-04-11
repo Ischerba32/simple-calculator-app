c94cfd50f56db320092a7465e2c3ad5246c4f7b8
Ischerba32 <i.scherba32@gmail.com>
2022-04-11 12:41:15 +0300

    added 0 and . handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-empty */
 import Calculator from './calculator';
 import Receiver from './receiver/receiver';
 import './styles/styles.css';
 
-const digits = document.querySelectorAll('.digit');
-const operations = document.querySelectorAll('.calc-keys button:not(.digit, .equal-sign)');
+const digits = document.querySelectorAll('.digit, .decimal');
+const operations = document.querySelectorAll('.calc-keys button:not(.digit, .equal-sign, .decimal)');
 const equal = document.querySelector('.equal-sign');
 const screen = document.querySelector('.calc-screen .initial');
 
@@ -13,19 +14,36 @@ const receiver = new Receiver(calculator);
 digits.forEach((digit) => {
 	digit.addEventListener('click', (e) => {
 		if (!receiver.operator && !receiver.rightOperand && !receiver.finish) {
-			receiver.handleLeftOperand(e.target.value);
-			screen.value = receiver.leftOperand;
-			console.log(receiver.leftOperand);
+			// handle 0 before "."
+			if (digit.value === '0' && !receiver.leftOperand.includes('.') && receiver.leftOperand === '0') {
+				receiver.leftOperand = digit.value;
+				screen.value = receiver.leftOperand;
+				console.log(receiver.leftOperand);
+			} else if (digit.value === '.' && receiver.leftOperand.includes('.')) {
+
+			} else {
+				receiver.handleLeftOperand(digit.value);
+				screen.value = receiver.leftOperand;
+				console.log(receiver.leftOperand);
+			}
 		} else if (receiver.leftOperand && receiver.rightOperand && receiver.finish) {
 			receiver.clearUI();
 
-			receiver.handleLeftOperand(e.target.value);
+			receiver.handleLeftOperand(digit.value);
 			screen.value = receiver.leftOperand;
 		} else if (receiver.operator && !receiver.finish) {
-			receiver.handleRightOperand(e.target.value);
-			screen.value = receiver.rightOperand;
+			if (digit.value === '0' && !receiver.rightOperand.includes('.') && receiver.rightOperand === '0') {
+				receiver.rightOperand = digit.value;
+				screen.value = receiver.rightOperand;
+				console.log(receiver.rightOperand);
+			} else if (digit.value === '.' && receiver.rightOperand.includes('.')) {
 
-			console.log(receiver.rightOperand);
+			} else {
+				receiver.handleRightOperand(digit.value);
+				screen.value = receiver.rightOperand;
+
+				console.log(receiver.rightOperand);
+			}
 		}
 	});
 });
@@ -54,6 +72,6 @@ equal.addEventListener('click', () => {
 	console.log(calculator);
 });
 
-// TODO: handling 0, ".", 0s after "."
 // TODO: commands like "left operand - operator"
 // TODO: calculator memory implementation
+// TODO: execute when clicked operand after executing operation
